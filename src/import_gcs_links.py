52f8c0fa7eb8d5ab90c66a72bf0f4d91e36049a3
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-10-30 18:34:00 +0300

    download from GCS (not tested, no permissions)

--- src/import_gcs_links.py
+++ src/import_gcs_links.py
@@ -53,25 +53,37 @@ def preview_csv(api: sly.Api, task_id, context, state, app_logger):
         sly.fs.silent_remove(local_path)
 
 
-def download_gcp_image(storage_client, remote_path, local_path):
+def download_gcp_image(gcs_client, remote_path, local_path):
     p = pathlib.Path(remote_path)
     bucket_name = p.parts[2]
     source_blob_name = os.path.join(*p.parts[3:])
 
-    bucket = storage_client.bucket(bucket_name)
+    bucket = gcs_client.bucket(bucket_name)
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(local_path)
+
+@my_app.callback("stop")
+@sly.timeit
+def stop(api: sly.Api, task_id, context, state, app_logger):
+    remote_path = "/temp/{}/".format(task_id)
+    api.file.remove(TEAM_ID, remote_path)
+    api.task.set_field(task_id, "data.finished", True)
 
 @my_app.callback("validate_creds")
 @sly.timeit
 def validate_creds(api: sly.Api, task_id, context, state, app_logger):
     api.file.download(TEAM_ID, state["credsPath"], gs_key_local_path)
-
     first_url = links_dict[0][state["urlColumn"]]
     first_url = first_url.replace("gs://", "https://storage.cloud.google.com/")
-    storage_client = storage.Client.from_service_account_json(gs_key_local_path)
+    file_name = sly.fs.get_file_name_with_ext(first_url)
+    gcs_client = storage.Client.from_service_account_json(gs_key_local_path)
+    preview_image_local_path = os.path.join(my_app.data_dir, file_name)
+    download_gcp_image(gcs_client, first_url, preview_image_local_path)
 
-    x = 10
+    remote_path = "/temp/{}/{}".format(task_id, file_name)
+    file_info = api.file.upload(TEAM_ID, preview_image_local_path, remote_path)
+
+    sly.fs.silent_remove(preview_image_local_path)
     pass
 
 def main():
@@ -80,7 +92,8 @@ def main():
         "csvDownloadError": "",
         "credError": "",
         "otherColumnsActions": [ACTION_IGNORE, ACTION_ADD_TO_META, ACTION_ADD_TO_TAGS],
-        "previewImageUrl": ""
+        "previewImageUrl": "",
+        "finished": False
     }
 
     state = {
