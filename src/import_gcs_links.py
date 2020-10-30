7a0fb26e82e444b0185c3ccbbcb7a17138b7149f
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-10-30 16:05:12 +0300

    handle download error on preview

--- src/import_gcs_links.py
+++ src/import_gcs_links.py
@@ -1,7 +1,8 @@
 import os
 import csv
+import requests
 import supervisely_lib as sly
-
+import json
 
 my_app = sly.AppService()
 
@@ -17,16 +18,28 @@ def _download_csv_file(api, remote_path):
 @sly.timeit
 def preview_csv(api: sly.Api, task_id, context, state, app_logger):
     remote_path = state["csvPath"]
-    local_path = _download_csv_file(api, remote_path)
-    with open(local_path) as f:
-        links = list(csv.reader(f))
-    table = {
-        "columns": links[0],
-        "data": links[1:5+1]
-    }
-    fields = [{"field": "data.previewTable", "payload": table},]
+    downloaded = False
+    error_text = ""
+    try:
+        local_path = _download_csv_file(api, remote_path)
+        downloaded = True
+    except requests.exceptions.HTTPError as e:
+        error_text = json.loads(e.response.text)
+
+    table = {"columns": [], "data": []}
+    if downloaded is True:
+        with open(local_path) as f:
+            links = list(csv.reader(f))
+        table = {
+            "columns": links[0],
+            "data": links[1:5+1]
+        }
+
+    fields = [{"field": "data.previewTable", "payload": table},
+              {"field": "data.csvDownloadError", "payload": error_text},]
     api.app.set_fields(task_id, fields)
-    sly.fs.silent_remove(local_path)
+    if downloaded is True:
+        sly.fs.silent_remove(local_path)
 
 
 @my_app.callback("preprocessing")
@@ -37,17 +50,19 @@ def preprocessing(api: sly.Api, task_id, context, state, app_logger):
 
 def main():
     data = {
-        "previewTable": {"columns": [], "data": []}
+        "previewTable": {"columns": [], "data": []},
+        "csvDownloadError": ""
     }
 
     state = {
-        "csvPath": "/my_folder/links.csv" #@TODO: set back to empty""
+        "csvPath": "/my_folder/links1.csv"
     }
 
     initial_events = [
         {"state": None, "context": None, "command": "preprocessing"}
     ]
 
+    #@TODO: csvPath -  set back to empty ""
     # Run application service
     my_app.run(data=data, state=state, initial_events=initial_events)
 
