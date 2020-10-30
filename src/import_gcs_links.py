b3da81a6123e10a786e5693d1ac7db3a33acad95
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-10-30 17:01:14 +0300

    columns actions

--- src/import_gcs_links.py
+++ src/import_gcs_links.py
@@ -5,6 +5,11 @@ import supervisely_lib as sly
 import json
 
 my_app = sly.AppService()
+links_dict = None
+
+ACTION_IGNORE = "ignore"
+ACTION_ADD_TO_META = "add to image as meta information"
+ACTION_ADD_TO_TAGS = "add to image as tags"
 
 TEAM_ID = int(os.environ['context.teamId'])
 
@@ -17,6 +22,7 @@ def _download_csv_file(api, remote_path):
 @my_app.callback("preview_csv")
 @sly.timeit
 def preview_csv(api: sly.Api, task_id, context, state, app_logger):
+    global links_dict
     remote_path = state["csvPath"]
     downloaded = False
     error_text = ""
@@ -29,42 +35,43 @@ def preview_csv(api: sly.Api, task_id, context, state, app_logger):
     table = {"columns": [], "data": []}
     if downloaded is True:
         with open(local_path) as f:
+            links_dict = list(csv.DictReader(f))
+        with open(local_path) as f:
             links = list(csv.reader(f))
-        table = {
-            "columns": links[0],
-            "data": links[1:5+1]
-        }
+        table = {"columns": links[0], "data": links[1:5+1]}
 
     fields = [{"field": "data.previewTable", "payload": table},
               {"field": "data.csvDownloadError", "payload": error_text},]
+
+    if downloaded is True:
+        fields.append({"field": "state.urlColumn", "payload": links[0][0]}),
     api.app.set_fields(task_id, fields)
     if downloaded is True:
         sly.fs.silent_remove(local_path)
 
-
-@my_app.callback("preprocessing")
-@sly.timeit
-def preprocessing(api: sly.Api, task_id, context, state, app_logger):
-    sly.logger.info("do something here")
-
+# @my_app.callback("preview_csv")
+# @sly.timeit
+# def preview_csv(api: sly.Api, task_id, context, state, app_logger):
+#     pass
 
 def main():
     data = {
         "previewTable": {"columns": [], "data": []},
-        "csvDownloadError": ""
+        "csvDownloadError": "",
+        "credError": "",
+        "otherColumnsActions": [ACTION_IGNORE, ACTION_ADD_TO_META, ACTION_ADD_TO_TAGS]
     }
 
     state = {
-        "csvPath": "/my_folder/links1.csv"
+        "csvPath": "/my_folder/links.csv",
+        "credsPath": "/my_folder/abc-b017125670ed.json",
+        "urlColumn": "",
+        "otherColumnsAction": ACTION_IGNORE
     }
 
-    initial_events = [
-        {"state": None, "context": None, "command": "preprocessing"}
-    ]
-
-    #@TODO: csvPath -  set back to empty ""
+    #@TODO: csvPath, credsPath -  set back to empty ""
     # Run application service
-    my_app.run(data=data, state=state, initial_events=initial_events)
+    my_app.run(data=data, state=state)
 
 
 if __name__ == "__main__":
