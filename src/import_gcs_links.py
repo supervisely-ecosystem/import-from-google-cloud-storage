3b941662f4140749fd7eddfa3d0ce4624526d9f2
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-11-02 09:55:18 +0200

    image preview works

--- src/import_gcs_links.py
+++ src/import_gcs_links.py
@@ -4,11 +4,13 @@ import requests
 import supervisely_lib as sly
 import json
 import pathlib
+import random
 import google.api_core.exceptions as google_exceptions
 from google.cloud import storage
 
 my_app = sly.AppService()
-links_dict = None
+links = None # list of dicts
+previewLinks = None # list of lists (raw csv)
 gs_key_local_path = os.path.join(my_app.data_dir, "key.json")
 
 ACTION_IGNORE = "ignore"
@@ -17,16 +19,26 @@ ACTION_ADD_TO_TAGS = "add to image as tags"
 
 TEAM_ID = int(os.environ['context.teamId'])
 
+
 def _download_csv_file(api, remote_path):
     local_path = os.path.join(my_app.data_dir, "/files/file.csv")
     sly.fs.ensure_base_path(local_path)
     api.file.download(TEAM_ID, remote_path, local_path)
     return local_path
 
+
+@my_app.callback("stop")
+@sly.timeit
+def stop(api: sly.Api, task_id, context, state, app_logger):
+    remote_path = "/temp/{}/".format(task_id)
+    api.file.remove(TEAM_ID, remote_path)
+    api.task.set_field(task_id, "data.finished", True)
+
+
 @my_app.callback("preview_csv")
 @sly.timeit
 def preview_csv(api: sly.Api, task_id, context, state, app_logger):
-    global links_dict
+    global links, previewLinks
     remote_path = state["csvPath"]
     downloaded = False
     error_text = ""
@@ -39,19 +51,41 @@ def preview_csv(api: sly.Api, task_id, context, state, app_logger):
     table = {"columns": [], "data": []}
     if downloaded is True:
         with open(local_path) as f:
-            links_dict = list(csv.DictReader(f))
+            links = list(csv.DictReader(f))
         with open(local_path) as f:
-            links = list(csv.reader(f))
-        table = {"columns": links[0], "data": links[1:5+1]}
+            previewLinks = list(csv.reader(f))
+        table = {"columns": previewLinks[0], "data": previewLinks[1:5+1]}
 
     fields = [{"field": "data.previewTable", "payload": table},
               {"field": "data.csvDownloadError", "payload": error_text},]
 
     if downloaded is True:
-        fields.append({"field": "state.urlColumn", "payload": links[0][0]}),
+        fields.append({"field": "state.urlColumn", "payload": previewLinks[0][0]}),
     api.app.set_fields(task_id, fields)
     if downloaded is True:
         sly.fs.silent_remove(local_path)
+
+def _transform(uri, state):
+    if state["transformUri"] is True:
+        res = uri.replace(state["suffixBefore"], state["suffixAfter"])
+        return res
+    else:
+        return uri
+
+@my_app.callback("transform_uri")
+@sly.timeit
+def transform_uri(api: sly.Api, task_id, context, state, app_logger):
+    columns = previewLinks[0]
+    uri_index = columns.index(state["urlColumn"])
+    results = []
+    for row in previewLinks[1:5 + 1]:
+        new_row = row.copy()
+        new_row[uri_index] = _transform(new_row[uri_index], state)
+        results.append(new_row)
+
+    table = {"columns": columns, "data": results}
+    fields = [{"field": "data.transformedTable", "payload": table}]
+    api.app.set_fields(task_id, fields)
 
 
 def download_gcp_image(gcs_client, remote_path, local_path):
@@ -63,12 +97,6 @@ def download_gcp_image(gcs_client, remote_path, local_path):
     blob = bucket.blob(source_blob_name)
     blob.download_to_filename(local_path)
 
-@my_app.callback("stop")
-@sly.timeit
-def stop(api: sly.Api, task_id, context, state, app_logger):
-    remote_path = "/temp/{}/".format(task_id)
-    api.file.remove(TEAM_ID, remote_path)
-    api.task.set_field(task_id, "data.finished", True)
 
 @my_app.callback("validate_creds")
 @sly.timeit
@@ -76,25 +104,25 @@ def validate_creds(api: sly.Api, task_id, context, state, app_logger):
     api.task.set_field(task_id, "data.credError", "")
 
     api.file.download(TEAM_ID, state["credsPath"], gs_key_local_path)
-    #first_url = links_dict[0][state["urlColumn"]]
-    first_url = "gs://favorita-to-be-annotated-images/favorita-1002-1003-alimentosmascotas/articulo/17800171243/1828470-IMG_20200217_182325890.jpg"
-    first_url = first_url.replace("gs://", "https://storage.cloud.google.com/")
-    file_name = sly.fs.get_file_name_with_ext(first_url)
+    rand_url = random.choice(links)[state["urlColumn"]]
+    rand_url = _transform(rand_url, state)
+    file_name = sly.fs.get_file_name_with_ext(rand_url)
     gcs_client = storage.Client.from_service_account_json(gs_key_local_path)
     preview_image_local_path = os.path.join(my_app.data_dir, file_name)
     try:
-        download_gcp_image(gcs_client, first_url, preview_image_local_path)
+        download_gcp_image(gcs_client, rand_url, preview_image_local_path)
     #except google_exceptions.GoogleAPICallError as e:
     except Exception as e:
-        api.task.set_field(task_id, "data.credError", str(e))
+        api.task.set_field(task_id, "data.credError", rand_url + " : " + str(e))
         return
-
     remote_path = "/temp/{}/{}".format(task_id, file_name)
-    #upload-replace api method?
+    api.file.remove(TEAM_ID, remote_path)
     file_info = api.file.upload(TEAM_ID, preview_image_local_path, remote_path)
-
     sly.fs.silent_remove(preview_image_local_path)
+
+    api.task.set_field(task_id, "data.previewImageUrl", file_info.full_storage_url)
     pass
+
 
 def main():
     data = {
@@ -103,20 +131,28 @@ def main():
         "credError": "",
         "otherColumnsActions": [ACTION_IGNORE, ACTION_ADD_TO_META, ACTION_ADD_TO_TAGS],
         "previewImageUrl": "",
-        "finished": False
+        "finished": False,
+        "transformedTable": {"columns": [], "data": []},
     }
 
     state = {
         "csvPath": "/my_folder/links.csv",
-        #"credsPath": "/my_folder/abc-b017125670ed.json",
         "credsPath": "/my_folder/nas-60e9c63d45f8.json",
         "urlColumn": "",
-        "otherColumnsAction": ACTION_IGNORE
+        "otherColumnsAction": ACTION_IGNORE,
+        "transformUri": True,
+        "suffixBefore": "gs://",
+        "suffixAfter": "https://storage.cloud.google.com/favorita-to-be-annotated-images/" #"https://storage.cloud.google.com/",
     }
 
     #@TODO: csvPath, credsPath -  set back to empty ""
-    # Run application service
     #@TODO: not found images exist!!! handle case
+    # @TODO api.file upload-replace api method?
+    # @TODO:  doc about bucket name in replace suffix
+    # LOW @TODO: add fields config to speedup multiple runs with save arguments
+    #@TODO: readme error description: does not have storage.objects.get access to the Google Cloud Storage object.: ('Request failed with status code', 403, 'Expected one of', <HTTPStatus.OK: 200>, <HTTPStatus.PARTIAL_CONTENT: 206>)
+
+    # Run application service
     my_app.run(data=data, state=state)
 
 
