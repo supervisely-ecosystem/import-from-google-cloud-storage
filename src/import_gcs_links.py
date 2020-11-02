c77c1d1d9bec1ac154d1322330c5c293b3a75afa
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-11-02 11:22:05 +0200

    upload in progress

--- src/import_gcs_links.py
+++ src/import_gcs_links.py
@@ -12,6 +12,7 @@ my_app = sly.AppService()
 links = None # list of dicts
 previewLinks = None # list of lists (raw csv)
 gs_key_local_path = os.path.join(my_app.data_dir, "key.json")
+gcs_client = None
 
 ACTION_IGNORE = "ignore"
 ACTION_ADD_TO_META = "add to image as meta information"
@@ -101,6 +102,7 @@ def download_gcp_image(gcs_client, remote_path, local_path):
 @my_app.callback("validate_creds")
 @sly.timeit
 def validate_creds(api: sly.Api, task_id, context, state, app_logger):
+    global gcs_client
     api.task.set_field(task_id, "data.credError", "")
 
     api.file.download(TEAM_ID, state["credsPath"], gs_key_local_path)
@@ -123,6 +125,79 @@ def validate_creds(api: sly.Api, task_id, context, state, app_logger):
     api.task.set_field(task_id, "data.previewImageUrl", file_info.full_storage_url)
     pass
 
+@my_app.callback("upload")
+@sly.timeit
+def upload(api: sly.Api, task_id, context, state, app_logger):
+    api.task.set_field(task_id, "data.uploadError", "")
+
+    workspace_name = state["workspaceName"]
+    if not workspace_name:
+        api.task.set_field(task_id, "data.uploadError", "Workspace name is not defined")
+        return
+
+    project_name = state["projectName"]
+    if not project_name:
+        api.task.set_field(task_id, "data.uploadError", "Project name is not defined")
+        return
+
+    dataset_name = state["datasetName"]
+    if not dataset_name:
+        api.task.set_field(task_id, "data.uploadError", "Dataset name is not defined")
+        return
+
+    workspace = api.workspace.get_info_by_name(TEAM_ID, workspace_name)
+    if workspace is None:
+        workspace = api.workspace.create(TEAM_ID, workspace_name)
+
+    project = api.project.get_info_by_name(workspace.id, project_name)
+    if project is None:
+        project = api.project.create(workspace.id, project_name)
+
+    dataset = api.dataset.get_info_by_name(project.id, dataset_name)
+    if dataset is None:
+        dataset = api.dataset.create(project.id, dataset_name)
+
+    if gcs_client is None:
+        api.task.set_field(task_id, "data.uploadError", "Error: Validate creds step is skipped")
+        return
+
+    for link in links:
+        uri = link[state["urlColumn"]]
+
+        res_url = _transform(uri, state)
+        file_name = sly.fs.get_file_name_with_ext(res_url)
+
+        ext = sly.fs.get_file_ext(file_name)
+        if not ext:
+            file_name += sly.image.DEFAULT_IMG_EXT
+
+        image_info = api.image.get_info_by_name(dataset.id, file_name)
+        need_upload = True
+        upload_name = file_name
+        if image_info is not None:
+            if state["skipImage"] is True:
+                need_upload = False
+            else:
+                upload_name = api.image.get_free_name(dataset.id, file_name)
+
+
+        local_path = os.path.join(my_app.data_dir, file_name)
+        try:
+            download_gcp_image(gcs_client, res_url, local_path)
+        # except google_exceptions.GoogleAPICallError as e:
+        except Exception as e:
+            app_logger.warn("Lisk {!r} skipped: {}".format(uri, str(e)))
+            return
+
+        if state["normalizeExif"] is True or state["removeAlphaChannel"] is True:
+            img = sly.image.read(local_path, remove_alpha_channel=state["removeAlphaChannel"])
+            sly.image.write(local_path, img, remove_alpha_channel=state["removeAlphaChannel"])
+
+        if need_upload is True:
+            api.image.upload_path(dataset.id, upload_name, local_path, meta={})
+            
+        sly.fs.silent_remove(local_path)
+
 
 def main():
     data = {
@@ -133,6 +208,7 @@ def main():
         "previewImageUrl": "",
         "finished": False,
         "transformedTable": {"columns": [], "data": []},
+        "uploadError": "",
     }
 
     state = {
@@ -142,7 +218,13 @@ def main():
         "otherColumnsAction": ACTION_IGNORE,
         "transformUri": True,
         "suffixBefore": "gs://",
-        "suffixAfter": "https://storage.cloud.google.com/favorita-to-be-annotated-images/" #"https://storage.cloud.google.com/",
+        "suffixAfter": "https://storage.cloud.google.com/favorita-to-be-annotated-images/", #"https://storage.cloud.google.com/",
+        "workspaceName": "delme", #"",
+        "projectName": "delme", #"",
+        "datasetName": "delme", #"",
+        "normalizeExif": True,
+        "removeAlphaChannel": True,
+        "skipImage": True
     }
 
     #@TODO: csvPath, credsPath -  set back to empty ""
@@ -151,7 +233,9 @@ def main():
     # @TODO:  doc about bucket name in replace suffix
     # LOW @TODO: add fields config to speedup multiple runs with save arguments
     #@TODO: readme error description: does not have storage.objects.get access to the Google Cloud Storage object.: ('Request failed with status code', 403, 'Expected one of', <HTTPStatus.OK: 200>, <HTTPStatus.PARTIAL_CONTENT: 206>)
-
+    #@TODO: normalize exif, remove alpha channel
+    #@TODO: add upload by link option
+    #@TODO: csv columns as meta or tags
     # Run application service
     my_app.run(data=data, state=state)
 
